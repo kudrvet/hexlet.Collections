e92a5080ad614d2642dfb6d0bb1d040a84c6a384
vet <kudrvet@gmail.com>
2017-03-31 16:17:51 +0400

    done m1test

---
+++ src/m1test/ArrayCollection.java
@@ -0,0 +1,159 @@
+package m1test;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+import java.util.Iterator;
+
+public class ArrayCollection<T> implements Collection<T> {
+
+    private T[] m = (T[])new Object[1];
+
+    private int size;
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        for (int i = 0; i < size; i++) {
+            if (m[i].equals(o)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ElementsIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        final T[] newM = (T[])new Object[this.size()];
+        System.arraycopy(m, 0, newM, 0, this.size());
+        return newM;
+    }
+
+    @Override
+    public <T1> T1[] toArray(final T1[] a) {
+        // BEGIN (write your solution here)
+        if (size()<=a.length ) {
+             System.arraycopy(m,0,a,0,size());
+             return a;
+        }
+        else {
+            return (T1[])Arrays.copyOf(this.m,this.size(),a.getClass());
+        }
+       // END
+    }
+
+    @Override
+    public boolean add(final T t) {
+        if (m.length == size) {
+            final T[] oldM = m;
+            m = (T[]) new Object[this.size() * 2];
+            System.arraycopy(oldM, 0, m, 0, oldM.length);
+        }
+        m[size++] = t;
+        return true;
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        for (int i = 0; i < size(); i++) {
+            if (m[i].equals(o)) {
+                this.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> c) {
+        for (final Object item : c) {
+            if (!this.contains(item)) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends T> c) {
+        for (final T item : c) {
+            add(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        for (final Object item : c) {
+            remove(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        for (final Object item : this) {
+            if (!c.contains(item)) this.remove(item);
+        }
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        m = (T[])new Object[1];
+        size = 0;
+    }
+
+    private void remove(final int index) {
+        if (index != this.size() - 1)
+            System.arraycopy(m, index + 1, m, index, this.size() - index - 1);
+        size--;
+    }
+
+    private class ElementsIterator implements Iterator<T> {
+
+        private int index;
+
+        private int last = -1;
+
+        @Override
+        public boolean hasNext() {
+            return ArrayCollection.this.size() > index;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            last = index;
+            return ArrayCollection.this.m[index++];
+        }
+
+        public void remove() {
+            if (last == -1)
+                throw new IllegalStateException();
+            ArrayCollection.this.remove(last);
+            index--;
+            last = -1;
+        }
+
+    }
+
+
+}
+
---
+++ src/m1test/ArrayCollectionTest.java
@@ -0,0 +1,219 @@
+package m1test;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class ArrayCollectionTest {
+
+    @Test
+    public void testSizeWhenSizeIs0() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        assertEquals(0, testInstance.size());
+    }
+
+    @Test
+    public void testIsEmptyWhenEmpty() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        assertTrue(testInstance.isEmpty());
+    }
+
+    @Test
+    public void testToArrayWhenInputArrayHaveSizeOne() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+        testInstance.add(3);
+
+        final Integer[] input = new Integer[1];
+
+        final Integer[] result = testInstance.toArray(input);
+        assertNotEquals(input, result);
+        assertEquals((Integer)1, result[0]);
+        assertEquals((Integer)2, result[1]);
+        assertEquals((Integer)3, result[2]);
+        assertEquals(3, result.length);
+    }
+
+    @Test
+    public void testToArrayWhenInputArrayHaveCorrectSize() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+        testInstance.add(3);
+
+        final Integer[] input = new Integer[3];
+
+        final Integer[] result = testInstance.toArray(input);
+        assertEquals(input, result);
+    }
+
+    @Test
+    public void testContains() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+
+        assertTrue(testInstance.contains(1));
+        assertFalse(testInstance.contains(0));
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(1);
+
+        assertEquals(2, testInstance.size());
+        assertFalse(testInstance.isEmpty());
+    }
+
+    @Test
+    public void testRemoveFirstElement() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+        testInstance.remove(1);
+
+        assertEquals(1, testInstance.size());
+        assertFalse(testInstance.isEmpty());
+    }
+
+    @Test
+    public void testRemoveLastElement() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+        testInstance.remove(2);
+
+        assertEquals(1, testInstance.size());
+        assertFalse(testInstance.isEmpty());
+    }
+
+    @Test
+    public void testContainsAll() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        final Collection<Integer> testInstance2 = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+
+        testInstance2.add(2);
+        testInstance2.add(1);
+
+        assertTrue(testInstance.containsAll(testInstance2));
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+
+        testInstance.add(3);
+        testInstance.add(4);
+
+        assertTrue(testInstance.contains(3));
+        assertTrue(testInstance.contains(4));
+    }
+
+    @Test
+    public void testRemoveAll() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        final Collection<Integer> testInstance2 = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+
+        testInstance2.add(2);
+        testInstance2.add(3);
+
+        testInstance.removeAll(testInstance2);
+
+        assertEquals(1, testInstance.size());
+        assertTrue(testInstance.contains(1));
+    }
+
+    @Test
+    public void testRetainAll() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        final Collection<Integer> testInstance2 = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+
+        testInstance2.add(2);
+        testInstance2.add(3);
+
+        testInstance.retainAll(testInstance2);
+
+        assertEquals(1, testInstance.size());
+        assertTrue(testInstance.contains(2));
+    }
+
+    @Test
+    public void testClear() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(1);
+
+        testInstance.clear();
+
+        assertTrue(testInstance.isEmpty());
+        assertEquals(0, testInstance.size());
+    }
+
+    @Test
+    public void testRemoveBeforeNext() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(2);
+
+        final Iterator<Integer> iter = testInstance.iterator();
+        try {
+            iter.remove();
+            fail("remove do not throw the Exception when called before next");
+        } catch (final IllegalStateException e) {}
+    }
+
+    @Test
+    public void testNextOnEmptyCollection() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+
+        final Iterator<Integer> iter = testInstance.iterator();
+        iter.next();
+        iter.remove();
+        iter.next();
+        iter.remove();
+        try {
+            iter.next();
+            fail("next do not throw the Exception when no more ellements");
+        } catch (final java.util.NoSuchElementException e) {}
+    }
+
+    @Test
+    public void testRemoveTwoTimeInTheRow() throws Exception {
+        final Collection<Integer> testInstance = new ArrayCollection<>();
+        testInstance.add(1);
+        testInstance.add(2);
+
+        final Iterator<Integer> iter = testInstance.iterator();
+        iter.next();
+        iter.remove();
+        assertEquals("Expected collection size is 1, however actual is not", 1, testInstance.size());
+        try {
+            iter.remove();
+            fail("remove do not throw the Exception when called twice");
+        } catch (final IllegalStateException e) {}
+    }
+
+}
+
